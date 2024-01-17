e42d9110414c2aa9149762f8d262856a5b30f665
= <aerobs@umich.edu>
2024-01-17 11:07:59 -0800

    Learning about useState

--- src/strava/Strava.tsx
+++ src/strava/Strava.tsx
@@ -1,30 +1,37 @@
 import React from "react";
-import OAuth2Login from "react-simple-oauth2-login";
+import { useState } from "react";
 
 import "./Strava.css";
 
 function Strava() {
-  const stravaAuthUrl = "https://www.strava.com/oauth/authorize";
-  const clientIdProp = "client_id=3d9d020516649342f53163e6ae025eec5bc00e22";
-  const redirectUriProp = `redirect_uri=${encodeURIComponent("http://localhost:3000")}`;
-  const responseTypeProp = "response_type=code";
-  const scopeProp = "scope=read";
-  const onSuccess = (response: any) => {
-    console.log(response);
-    debugger;
+  //this.state = {stravaToken: localStorage.getItem('stavatoken') || null}
+  const [inputValue, setInputValue] = useState("");
+  const [stravaToken, setStravaToken] = useState(
+    localStorage.getItem("stravaToken") || "",
+  );
+
+  const handleChange = (event: any) => {
+    setInputValue(event?.target?.value);
   };
-  const onFailure = (response: any) => {
-    console.log(response);
-    debugger;
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    if (inputValue) {
+      localStorage.setItem("stravaToken", inputValue);
+      setStravaToken(inputValue);
+    }
   };
+
   return (
     <div className="strava">
-      <a
-        target="_blank"
-        href={`${stravaAuthUrl}?${clientIdProp}&${redirectUriProp}&${responseTypeProp}&${scopeProp}`}
-      >
-        Login
-      </a>
+      {!stravaToken ? (
+        <form onSubmit={handleSubmit}>
+          <input value={inputValue} onChange={handleChange} />
+          <button type="submit">Save token</button>
+        </form>
+      ) : (
+        <p>token: {stravaToken}</p>
+      )}
     </div>
   );
 }
